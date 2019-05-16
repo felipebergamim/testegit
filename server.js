92a353c3f98fbd9c7b387d0ad0e0472f8daeed13
felipebergamim <50702647+felipebergamim@users.noreply.github.com>
2019-05-16 09:04:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/',(req, res)=>{
   res.status(200).send({message: 'Ola mundo estou funcionando!'});
 })
 
+app.get('Ola',(req, res)=>{
+  res.status(200).send({message: 'Oi, eu sou a rota Ola'});
+})
+
 app.listen(3001, ()=>{
     console.log('Api rodando na porta 3001');
 })
